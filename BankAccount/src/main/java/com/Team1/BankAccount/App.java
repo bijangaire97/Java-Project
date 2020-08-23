95f89ca9c78d084a9b248f7cd6a5caa5c593a969
bijangaire97 <56812736+bijangaire97@users.noreply.github.com>
2020-08-23 18:50:48 -0500

    added user and user test file

---
+++ BankAccount/src/main/java/com/Team1/BankAccount/App.java
@@ -0,0 +1,13 @@
+package com.Team1.BankAccount;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
